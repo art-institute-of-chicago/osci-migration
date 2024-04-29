3faff4a464b55266517791bac5b6392709694a93
Charlie Butcosk <incharge@weichbild.com>
2024-04-29 14:54:35 -0400

    Refine title bits -- rename workflow, add buffer and align w/ readableStream docs

--- alignments/head-title.js
+++ alignments/head-title.js
@@ -1,17 +1,30 @@
 const { JSDOM } = require('jsdom');
 
-process.stdin.on("data", data => {
+(() => {
 
-  const html = data.toString()
-  const dom = new JSDOM(html)
+  process.stdin.once("readable", () => {
 
-  const t = dom.window.document.querySelector('head title')
-  if (t) {
-    console.log(JSON.stringify(t.textContent.trim()))
+    // Basically just the node docs reader (https://nodejs.org/api/stream.html#event-readable) so we're OK re: buffering large STDINs
+    // TODO: Return a promise or something so this can just be imported by other alignment scripts
+    let data;
+    let buf;
+
+    // p. sure this read() is fully buffered but node docs show while..
+    while ((data = process.stdin.read()) !== null) {
+      buf += data.toString()
+    }
+
+    const dom = new JSDOM(buf)
+
+    if (dom.window.document.title) {
+      const result = { 'head_title': dom.window.document.title.trim() }
+      process.stdout.write(JSON.stringify(result))
+      return 
+    }
+
+    process.stdout.write(JSON.stringify({}))
     return
-  }
 
-  console.log(JSON.stringify(''))
-  return
+  })
 
-})
+})()
